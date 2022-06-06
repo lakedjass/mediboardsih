fdbee712c804c13caf77a2d64d2b9e3cbbbaee0d
lakedjass <kkouamjjunior@gmail.com>
2022-06-06 13:52:05 +0100

    ok

---
+++ hadoop/check.sh
@@ -0,0 +1,10 @@
+ssh-keygen -t rsa -P '' -f ~/.ssh/id_rsa
+cat ~/.ssh/id_rsa.pub  >> ~/.ssh/authorized_keys
+chmod 0600 ~/.ssh/authorized_keys
+
+ssh localhost
+
+hdfs namenode -format
+start-dfs.sh
+start-yarn.sh
+jps
